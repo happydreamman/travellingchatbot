677c251789eae3173600ff84cf0c936b2dfb20a4
happydreamman <happydreamman@gmail.com>
2023-02-09 08:38:41 -0800

    update

--- app.py
+++ app.py
@@ -14,28 +14,27 @@ def index():
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=generate_prompt(animal),
-            max_tokens=200,
+            max_tokens=10,
             temperature=0.1,
         )
         print("response: ",response, "\n")
-        return redirect(url_for("index", result=animal + response.choices[0].text))
+        return redirect(url_for("index",result="Score: " + response.choices[0].text))
 
     result = request.args.get("result")
     # result ="a\nb\nc"
     print("result: ", result)
-    r0=r1=r2=r3=""   
-    if result and len(result.split("\n"))>=4:
-        r0 = result.split("\n")[0]
-        r1 = result.split("\n")[1]
-        r2 = result.split("\n")[2]
-        r3 = result.split("\n")[3]
-    return render_template("index.html", result=result, r0=r0,r1=r1,r2=r2,r3=r3)
+    # r0=r1=r2=r3=""   
+    # if result and len(result.split("\n"))>=4:
+    #     r0 = result.split("\n")[0]
+    #     r1 = result.split("\n")[1]
+    #     r2 = result.split("\n")[2]
+    #     r3 = result.split("\n")[3]
+    return render_template("index.html", result=result)
 
 
 def generate_prompt(animal):
-    return """Suggest the best 3 airline paths to travel to New York, USA from my location? And how long will it take? 
+    return """Answer me with intelligent q. What is the score of the statement below out of 10?
 
-Location: {}
-Path:""".format(
+{}""".format(
         animal.strip()
     )
